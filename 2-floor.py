2d03d0af34a5a916e7e903b0518b4eb3ebaa2c85
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-01-06 18:01:13 +0100

    chore/add-task-2

---
+++ 2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This is part of 0x00-python_variable_annotations
+"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """takes a float number and returns the floored version of the float
+
+    Args:
+        n: float
+
+    Return:
+        float
+    """
+    return math.floor(n)
