c42b5da78c71b323bd6100f919fde5db85131f99
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-01-06 18:00:57 +0100

    chore/add-task-1

---
+++ 1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This is part of 0x00-python_variable_annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """add tow strings and returns another string
+
+    Args:
+        str1: string
+        str2: string
+
+    Return:
+        string
+    """
+    return str1 + str2
