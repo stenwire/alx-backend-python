0d102aeeb29868d10af815ff679658e1faeae1d3
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-01-06 18:00:04 +0100

    fix/fix-syntax-add-docs

--- 0-add.py
+++ 0-add.py
@@ -1,3 +1,17 @@
-#!/usr/bin/python3
-def add(a:float, b:float) -> float:
-  return a + b
+#!/usr/bin/env python3
+"""
+This is part of 0x00-python_variable_annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """add tow float numbers and returns a result in float
+
+    Args:
+        a: float
+        b: float
+
+    Return:
+        float
+    """
+    return a + b
