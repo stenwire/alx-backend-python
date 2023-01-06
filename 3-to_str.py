a8997bdfef42b20087e9cc6db2c7b96758798fb6
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2023-01-06 18:01:29 +0100

    chore/add-task-3

---
+++ 3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This is part of 0x00-python_variable_annotations
+"""
+
+
+def to_str(n: float) -> str:
+    """Accepts a variable and returns a string
+
+    Args:
+        n: float
+
+    Return:
+        string
+    """
+    return str(n)
